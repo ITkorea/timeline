88470b68712256a46c83694e2f6169f6a89f2d20
ITkorea <davisasan@hanmail.net>
2015-04-13 10:51:40 +0900

    타임라인 글 삭제 페이지 만들기

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -29,7 +29,11 @@ class WallController < ApplicationController
       redirect_to :back
      end
    end
-   
+
+
+   def delete
+     @post_delete = Post.find(params[:id]) 
+   end
 
   def posts
 		@posts = Post.all
